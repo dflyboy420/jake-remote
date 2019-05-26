69702a8e702454da02ce566893861d8da31f9d5a
dflyboy420 <dflyboy420@socialmediaulm.de>
2019-05-26 23:19:13 +0200

    updating compiler class

--- compiler.js
+++ compiler.js
@@ -2,63 +2,37 @@ const fs = require("fs");
 const {
     spawn
 } = require("child_process");
-const async = require("async");
-const path = require("path");
+
 const logger = require("./logger");
+const Document = require("./document");
 
 class Compiler {
     /**
      * Construct a new JAKE compiler instance
-     * @param {string[]} files Array of files as returned by multer
-     * @param {string[]} arguments Command line arguments
-     * @param {*} id document id
+     * @param {Document} lillyDocument Document instance to compile
      */
-    constructor(files, args, id) {
-        this.rawFiles = files;
-        this.arguments = args;
-        this.folder = path.normalize(__dirname + "documents/" + id);
-        this.files = [];
-    }
-
-    async createDocumentFolder() {
-        fs.mkdir(this.folder, (err) => {
-            if (err) throw err;
-
-            async.each(this.rawFiles, (file, cb) => {
-                const dest = this.folder + path.sep + file.originalname;
-                fs.copyFile(file.path, dest, (err) => {
-                    if (err) cb(err);
-                    else {
-                        const regex = /^.*\.tex$/;
-                        if (regex.test(dest)) this.mainFile = dest;
-                        this.files.push(dest);
-                        cb();
-                    }
-                });
-            }, (err) => {
-                if (err) {
-                    throw new Error("Document files couldn't be copied");
-                } else {
-                    logger.info("Document folder for " + this.id + " was created");
-                }
-            });
-        });
+    constructor(lillyDocument) {
+        this.document = lillyDocument;
+        this.folder = lillyDocument.folder;
     }
 
     runJake() {
         return new Promise((resolve, reject) => {
-            if (!this.mainFile) throw new Error("no main file detected");
-            var args = this.arguments;
-            args.push(this.mainFile);
-            var jakeInstance = spawn("lilly_jake", this.arguments, {
+            let mainFile = this.document.mainFile;
+            let args = [mainFile];
+            var jakeInstance = spawn("lilly_jake", args, {
                 cwd: this.folder
             });
             jakeInstance.on("close", (code) => {
-                if(code !== 0) {
+                if (code !== 0) {
                     return reject("lilly_jake failed with code %d", code);
                 } else {
                     logger.info("Executed lilly_jake successfully on document %d", this.id);
-                    return resolve();
+                    this.document.update({
+                        compiled: true
+                    }).then(() => {
+                        return resolve();
+                    });
                 }
             });
         });
